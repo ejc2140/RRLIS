e89ed7adc996e6dffb06298b763897e36cdbcf5b
ejc2140 <ejc2140@g.rit.edu>
2022-02-26 20:09:07 -0500

    ????

--- rrlis/src/index.js
+++ rrlis/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Parent from './parent';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Parent />
   </React.StrictMode>,
   document.getElementById('root')
 );
